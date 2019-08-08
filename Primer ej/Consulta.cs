ba348541666df086cc8183d10cb4c5d3f003f17f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-08 07:41:53 -0700

    Agregar archivos de proyecto.

---
+++ Primer ej/Consulta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_ej
+{
+    class Consulta : Paciente 
+    {
+        Paciente paciente;
+        DateTime fecha;
+        string sintomas;
+        string diagnostico;
+        string tratamiento;
+    }
+}
---
+++ Primer ej/Paciente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_ej
+{
+    class Paciente
+    {
+        //Atributos (Variable)
+        string nombre;
+        string direccion;
+        string telefono;
+        DateTime fechaDeNacimiento;
+        string enfermedadesCronicas; 
+        
+        //metodos (funciones)
+
+    }
+}
